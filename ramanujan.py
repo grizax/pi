b07a87fd8d049a2f5c89eaa97b15dd7e4c489e67
Nick Foster <ndbfoster@gmail.com>
2015-04-22 19:52:39 -0400

    Computes an estimation of pi with the Srinivasa Ramanujan algorithm.

--- ramanujan.py
+++ ramanujan.py
@@ -1,10 +1,38 @@
 import math
 
+
 def factorial(n):
-		''' Computes the factorial of n '''
-		if n == 0:
-				return 1
-		else:
-				recurse = factorial(n - 1)
-				result = n * recurse
-				return result
+
+    ''' Computes the factorial of n '''
+
+    if n == 0:
+        return 1
+    else:
+        recurse = factorial(n - 1)
+        result = n * recurse
+        return result
+
+
+def pi():
+
+    '''
+    Computes an estimation of pi with the Srinivasa Ramanujan algorithm.
+    https://en.wikipedia.org/wiki/Pi#Rapidly_convergent_series
+    '''
+
+    total = 0
+    k = 0
+    first = 2 * math.sqrt(2) / 9801
+    while True:
+        num = factorial(4 * k) * (1103 + 26390 * k)
+        den = factorial(k)**4 * 396**(4 * k)
+        term = first * num / den
+        total += term
+
+        if abs(term) < 1e-15:
+            break
+        k += 1
+
+    return 1 / total
+
+print(pi())
